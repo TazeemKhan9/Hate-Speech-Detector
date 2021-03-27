d358d8aab979fad61e50c6ae1738b7e2c76bd322
Tazeem Khan <67814275+TazeemKhan9@users.noreply.github.com>
2021-03-27 15:21:52 +0530

    Update hate_speech_streamlit_app.py

--- hate_speech_streamlit_app.py
+++ hate_speech_streamlit_app.py
@@ -7,7 +7,8 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from swachhdata.text import *
 from keras.utils.data_utils import get_file
 import speech_recognition as sr 
-import os 
+import os
+import json
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
@@ -18,6 +19,20 @@ tokenizer =Tokenizer()
 r = sr.Recognizer()
 df=pd.read_csv("https://github.com/TazeemKhan9/Hate-Speech-Detector/blob/main/Data/cleaned_tweet.csv?raw=true")
 sentiment = ['Hate Speech','Offensive Language','No Issues']
+data={
+  "type": "service_account",
+  "project_id": "acoustic-arch-308907",
+  "private_key_id": "a25c1c779c6e690f84d5555a7040c250dc8384bd",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDF5inqmMM7riI3\nQrt0ZNpGNEeVyBzoGn1OHt24TmbHyY2t7cXZp03EI86RYu+8GcMlSC2glxMBS/GT\nGPOrIfMo9wPa1yWEiONy702TDhOo+hpaknePR/nycDbpQnS7ddZeLqKV8bdwBwnO\nr2PTR36NT79gd0xBn3eU+U6gVRZ8L3RJ6B5eyCsQbr+4JJLI+jA7Bdr+FuNXvGiG\nvDiFkayJyQCmImhEe5Rf2r9etxRBHa+e3FpcrZMQVK9kMQlrsZAa1q9w+bOkXIL2\nQrSayDH1KSqLNkvJq/ivp4alQjflDHjD6K1S8xSRHoza23Ip4h8PiMiiyGpmDzJG\nc9fspNaTAgMBAAECggEAK1lA2kHvFf4NQaSM8jB7PYCLI6yLZG4U/400TQBjdCTa\ns3wPaB9FIg8j2uXclIviGqMGV5RwFcsgVfPTwCs2G8WL4x5YegEQLWdsyyb8uHlc\nV3WY2dpOzkO1S4ACmON1evjlJUomv9PS2qLBj5CsEGElDwjiu8cmBwxbJDSvMbFC\nd28glOeWpDKtMwaJu9ZaPpcC8h6w9H2a9aKTRjZJMHUkpMFNaEEf/inCyLylef6J\n+7xvXz0UdOioe8+rD1ZrIGSSs9QqthhUgqVQTAfxSAXZ53jobgglARcStFbi5CEV\nDYAODR5AWaZPWS88JZc8aQ3igmocH3hOl4YT/5HkFQKBgQDy5ogPZi9sAnInRiKA\nFfJOIDB9X8a3SAlgyCUQu0jUzH3QDBofEofDECnA69Lor66sTepPRx/OwsXLt7o8\nbyN6/FWZKUaTN9oBeW9blA6psa3exRaXGCJSeCpCiF8bB3Z0CQGX881Q/1x0z4cF\nls475TkZjOU5P6/38B/VrDSe5QKBgQDQklhqs+/ggQenWZvFIy4msO2lAEjb+sDn\ntlwl5Q8+iGLzS0Y+TqCK5Jv6lD+6tc20uw135boRsqjfsEcQe+HYHcBvyPtHHdba\nffF6OZa50Bm5gwEUYH6sKOshFOoLru0+quwn8QhwNhgjPAeoE7npWR4JObPjvGRz\nqf89YgdQFwKBgQC54uZ5MnBULkMB/1BjyWfXlhbFu8gtdzmGEWUcOtdv0tbtonVT\nFjFDfFkXxOFxJRF911rbNMkIyFHqpz4lBcCXXAh93/Kcs39o5W/tG49lGg6/jwDM\nvLF3f3KH3Ck8XCewgTvw96lGtUYiNrdT9ab6e1+JSCQb/btC+UbDlLfoaQKBgQDH\n/Dqo/SU5N58WGHainKvoz2bd+hriSlnjE1jhwPNP+0gdjgSpQ4zuAGuK5dEBfsbh\nzyUH3I7/3zXLXeOV66LOLSDSTnyZYQQc9fuvPT7HpcC0vucvGaL8AjQJwVr0nuK+\nXvcXCScVKNkWF74jq95r31ZMdDaHW6FZwhuJSNBIOwKBgQDwh1KvfKG1uEADeuAc\nrdVb18rB/hHECuLq9pcobFR5fZTSaQOoffwmjbOxhdwgwSmjiI0yfrNQe0YmaFNo\nr4JExebdrqSR1z+Rx+T1skEh6Nz9NBkOXoL3KMpT4MWrmGemwlw+BRzXhFMd3KIf\nuigRkaoEoGsswIZfaCq2cZtdJA==\n-----END PRIVATE KEY-----\n",
+  "client_email": "speech-to-text@acoustic-arch-308907.iam.gserviceaccount.com",
+  "client_id": "104798248999236730114",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/speech-to-text%40acoustic-arch-308907.iam.gserviceaccount.com"
+}
+y=json.dumps(data)
+
 PAGE_CONFIG = {"page_title":"Hate Speech Detector","page_icon":":smiley:","layout":"centered"}
 st.set_page_config(**PAGE_CONFIG)
 
@@ -63,9 +78,11 @@ def get_large_audio_transcription(path):
             audio_listened = r.record(source)
             # try converting it to text
             try:
-                text = r.recognize_google(audio_listened)
-            except sr.UnknownValueError as e:
-                print("Error:", str(e))
+              text = r.recognize_google_cloud(audio_listened,credentials_json=y)
+            except sr.UnknownValueError:
+              print("Google Cloud Speech could not understand audio")
+            except sr.RequestError as e:
+              print("Could not request results from Google Cloud Speech service; {0}".format(e))
             else:
                 text = f"{text.capitalize()}. "
                 #print(chunk_filename, ":", text)
@@ -91,7 +108,7 @@ def main():
         user_input= get_large_audio_transcription(uploaded_file)
         st.subheader("Speech to Text Result")
         st.write(user_input)
-    if st.button('Generate Result'):
+    if st.button('Generate Text'):
       input = process(user_input)
       clean=df['tweet'].astype('str')
       tokenizer.fit_on_texts(clean.values)
